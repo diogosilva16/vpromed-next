f6a4276f874bb3a7459c41b2811daad4e7c4012f
Diogo Silva <diogosilva16@ua.pt>
2022-07-28 20:55:02 +0100

    --another vercel fix

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;600;700;800&display=swap"
             rel="stylesheet"
