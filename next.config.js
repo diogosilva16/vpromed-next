470e21e478dac08f99830ccaee099328bc4fee82
Diogo Silva <diogosilva16@ua.pt>
2022-08-05 18:10:56 +0100

    fixes 2.0

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  env: {
+    API_KEY: "t8rAzpkJR8O3kDZdw63h85GDrV86VOeX",
+  }
 }
 
 module.exports = nextConfig
